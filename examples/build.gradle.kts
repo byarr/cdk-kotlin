9cb12a7b5a3ef4fcb6bc4f7f3257c651bbc240a3
Brian Yarr <brian.yarr@gmail.com>
2019-12-30 18:51:50 +0000

    Add events-targets dep to examples

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -3,4 +3,5 @@ dependencies {
     implementation(project(":core"))
     implementation(project(":events"))
     implementation(project(":lambda"))
+    implementation("software.amazon.awscdk:events-targets")
 }
